7dcac06ac3d6b37cace78c9e7b9b6ca7cce24d17
Harsh Tiwari <harsh.official1802@gmail.com>
2024-03-31 01:28:04 +0530

    "fixed leak"

--- server/server.js
+++ server/server.js
@@ -1,23 +1,2 @@
-// const { default: mongoose } = require("mongoose");
-// const app = require("./index");
-// const dotenv = require("dotenv");
-
-// dotenv.config();
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://harshofficial1802:harsh1802@cluster0.21roddq.mongodb.net/book-harbor?retryWrites=true&w=majority",
-//     {
-//       useNewUrlParser: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("DB connection successful");
-//   });
-
-// const port = process.env.PORT || 3000;
-// const server = app.listen(port, () => {
-//   console.log(`Server is running on port ${port}....`);
-// });
 
 
