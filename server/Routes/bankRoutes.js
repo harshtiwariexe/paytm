fdc79bc623ff797eb8f1531b25214983487f865b
Harsh Tiwari <harsh.official1802@gmail.com>
2024-04-08 01:27:05 +0530

    "Basic Transcation Code(unoptimized ,Used for Ideal Conditions)"

--- server/Routes/bankRoutes.js
+++ server/Routes/bankRoutes.js
@@ -7,14 +7,14 @@ const Router = express.Router()
 
 Router.get('/balance',authMiddleware,async (req,res)=>{
     try {
-        const account = await Account.find({
+        const account = await Account.findOne({
             userId:req.userId
         })
         
         res.status(200).json({
             status:"success",
             data:{
-                account
+                balance:account.balance
             }
             
         })
@@ -23,4 +23,51 @@ Router.get('/balance',authMiddleware,async (req,res)=>{
     }
 
 })
+
+Router.post('/transfer',authMiddleware,async (req,res)=>{
+    try {
+        const {to,amount}=req.body;
+
+        const receiver = await User.findOne({
+            userId:to
+        })
+        if(!receiver){
+            res.status(400).json({
+                message:"Invalid Account"
+            })
+        }
+
+        const senderBalance = await Account.findOne({
+            userId:req.body.userId
+        })
+        if(senderBalance < amount){
+            res.status(400).json({
+                message:"Insufficient Balance"
+            })
+        }
+        
+        await Account.updateOne({
+            userId:req.body.userId,
+        },{
+            $inc:{
+                balance:-amount
+            }
+        })
+        await Account.updateOne({
+            userId:to,
+        },{
+            $inc:{
+                balance:amount
+            }
+        })
+        res.status(200).json({
+            message: "Transfer Succesfull"
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
 module.exports=Router
